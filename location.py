338cfb510e16a65d2403bdc47d6964c579b0440b
supersidd2001 <63630893+supersidd2001@users.noreply.github.com>
2023-02-10 11:12:35 +0530

    Add files via upload

--- location.py
+++ location.py
@@ -2,24 +2,23 @@
 #import datetime
 #import pymongo
 
-#client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+#client = pymongo.MongoClient('mongodb://localhost:5000/')
 #time1 = datetime.datetime.now().strftime('%d-%m-%Y')
-"""
-def loc():
-    g = geocoder.ip('me')
-    return g.latlng
 
-def record():
-    try:
-        time2 = datetime.datetime.now().strftime(' %H-%M-%S')
-        l = loc()
-        lat, long = l[0], l[1]
-        mydb = client["Data"]
-        inf = mydb.pothole
+#def loc():
+    #g = geocoder.ip('me')
+    #return g.latlng
+
+#def record():
+    #try:
+        #time2 = datetime.datetime.now().strftime(' %H-%M-%S')
+        #l = loc()
+        #lat, long = l[0], l[1]
+        #mydb = client["Data"]
+        #inf = mydb.pothole
         #for rec in inf.find():
             #if lat not in rec['latitude']:
-        rd = {'date': time1, 'time': time2, 'latitude': lat, 'longitude': long}
-        inf.insert_one(rd)
-    except:
-        pass
-"""
+                #rd = {'date': time1, 'time': time2, 'latitude': lat, 'longitude': long}
+        #inf.insert_one(rd)
+    #except:
+        #pass
